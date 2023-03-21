883f2f9786b9c8a91f6d3291e3429f35f9ca4318
SamiyalAndroid <onllinkswebservices@gmail.com>
2023-03-21 12:02:27 +0530

    ,dld

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(new MaterialApp(
-      home: new HomePage()
-  ));
+  runApp(new MaterialApp(home: new HomePage()));
 }
 
 class HomePage extends StatefulWidget {
@@ -16,42 +14,90 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-
-List? data;
+  List? data;
 
   Future<String> getData() async {
     var response = await http.get(
-        Uri.parse("https://jsonplaceholder.typicode.com/posts"),
-        headers: {
-          "Accept": "application/json"
-        }
-    );
+        Uri.parse(
+            "https://fireworks.galileosoft.com/api/product/getfeaturedproduct/1"),
+        headers: {"Accept": "application/json"});
 
     this.setState(() {
       data = json.decode(response.body);
     });
 
-    print(data![1]["title"]);
+    print(data![1]["name"]);
 
     return "Success!";
   }
 
   @override
-  void initState(){
+  void initState() {
     this.getData();
   }
 
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("Listviews"), backgroundColor: Colors.blue),
-      body: new ListView.builder(
-        itemCount: data == null ? 0 : data!.length,
-        itemBuilder: (BuildContext context, int index){
-          return new Card(
-            child: new Text(data![index]["title"]),
-          );
-        },
+  Widget build(BuildContext context) {
+    String Base_Url = "https://fireworks.galileosoft.com";
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar:
+          AppBar(title: new Text("Listviews"), backgroundColor: Colors.blue),
+      body: SafeArea(
+        child: Container(
+          height: height * 0.35,
+          width: width,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: data == null ? 0 : data!.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: height * 0.15,
+                width: width * 0.5,
+                margin: EdgeInsets.all(width*0.02),
+                child: Card(
+                  
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Container(
+                          child: Image.network(
+                              height: height * 0.20,
+                              Base_Url + data![index]["mainimg"]),
+                        ),
+                      ),
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, width * 0.05, 0, 0),
+                          child: Text(
+                            data![index]["name"],
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, width * 0.05, 0, 0),
+                          child: Text(
+                            "\$ " + data![index]["mainprice"].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w900,fontSize: width*0.04),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
